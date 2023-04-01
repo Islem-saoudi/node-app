9a3db902b6d758550a29f07ae3d7a7ade0ab648f
islem-saoudi <saoudisaoudi56@gmail.com>
2023-04-01 22:20:26 +0000

    routing

--- http.js
+++ http.js
@@ -1,10 +1,19 @@
 const http = require('node:http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('hello world');
+        res.end();
+    }
+    if (req.url === '/home') {
+        res.write ('welcome from home page');
+        res.end();
+    }
+});
 
-server.on('connection', (e) => {
-    console.log(`new connection`);
-})
+// server.on('connection', (e) => {
+//     console.log(`new connection`);
+// })
 
 server.listen(3000, () => console.log(`listening on port 3000 ...`));
  
