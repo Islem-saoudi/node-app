66d064e4d93a70f79f8253462ce69d7752006c73
islem-saoudi <saoudisaoudi56@gmail.com>
2023-04-02 20:09:36 +0000

    error

--- error_page.js
+++ error_page.js
@@ -5,10 +5,15 @@ const server = http.createServer((req, res) => {
         res.end(`<h1>Hello world</h1>
         <p>From home page</p>`);
     }
-    if (req.url === '/home') {
-        res.write ('welcome from about page');
-        res.end();
+
+    // the error is req.ulr not req.url
+
+    if (req.ulr === '/about') {
+        res.end('welcome from about page');
     }
+    res.end(`<h1>Error Page</h1>
+    <p>Page Not Found, To Go For Home Page Click <a href= "/">Here</a></p> `);
+
 });
 
 server.listen(3000, () => console.log(`listening on port 3000 ...`));
